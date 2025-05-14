107c8ffd6087d29625c97d0062ec922b5efaa72a
Prashant-Pubudu <100054944+Manojpu@users.noreply.github.com>
2025-05-14 18:06:04 +0530

    Refactor Token and LexicalAnalyzer classes for improved readability and functionality

--- rpal-compiler/lexer.py
+++ rpal-compiler/lexer.py
@@ -1,79 +1,78 @@
 class Token:
-    def __init__(self, type, value =None, line=0, column=0):
+    def __init__(self, type, value=None, line=0, column=0):
         self.type = type
         self.value = value
         self.line = line
         self.column = column
-    #for debugging purposes
-    def __repr__(self):
-        return f"Token({self.type},{self.value})"
-  
-    def __str__(self):
-        return f"<{self.type}:{self.value}>" if self.value else f"<{self.type}>"
-   
-class LexicalAnalyzer:
-    def __init__(self, text):
-        self.text = text
-        self.pos = 0
-        self.line = 1
-        self.column = 0
-        self.tokens = []
-        self.current_char = self.text[self.pos]
-        self.current_token = None
-        self.advance()
-
-    def error(self):
-        raise Exception('Invalid character')
-
-    def advance(self):
-        #Move to the next character in the text
-
-        self.pos += 1
-        if self.pos > len(self.text) - 1:
-            self.current_char = None
-        else:
-            self.current_char = self.text[self.pos]
-            self.column += 1
     
-    def peek(self,n=1):
+    def __str__(self):
+        if self.value:
+            return f"{self.type}:{self.value}"
+        return self.type
+
+class LexicalAnalyzer:
+    def __init__(self, input_text):
+        self.input_text = input_text
+        self.position = 0
+        self.line = 1
+        self.column = 1
+        self.tokens = []
+        self.current_char = None
+        self.advance()
+    
+    def advance(self):
+        """Move to the next character in the input"""
+        if self.position < len(self.input_text):
+            self.current_char = self.input_text[self.position]
+            self.position += 1
+            self.column += 1
+        else:
+            self.current_char = None
+    
+    def peek(self, n=1):
+        """Look ahead n characters without advancing"""
         peek_pos = self.position + n - 1
         if peek_pos < len(self.input_text):
             return self.input_text[peek_pos]
         return None
-
+    
     def skip_whitespace(self):
-        while self.current_char  and self.current_char.isspace():
+        """Skip whitespace characters"""
+        while self.current_char and self.current_char.isspace():
             if self.current_char == '\n':
                 self.line += 1
-                self.column = 0
+                self.column = 1
             self.advance()
-
+    
     def skip_comment(self):
+        """Skip comments starting with //"""
         while self.current_char and self.current_char != '\n':
             self.advance()
-
-    def skip_multiline_comment(self):
-        while self.current_char and (self.current_char != '*' or self.peek() != '/'):
+    
+    def handle_multi_line_comment(self):
+        """Skip multi-line comments enclosed in /* ... */"""
+        while self.current_char:
+            if self.current_char == '*' and self.peek() == '/':
+                self.advance()  # consume '*'
+                self.advance()  # consume '/'
+                return
             if self.current_char == '\n':
                 self.line += 1
-                self.column = 0
+                self.column = 1
             self.advance()
-        if self.current_char == '*':
-            self.advance()
-        if self.current_char == '/':
-            self.advance()
-        
-        if self.current_char == None:
-            raise Exception('Unterminated multiline comment')
-        self.advance()
-
-
-    def identifier(self):
+        # If we get here, the comment was not properly closed
+        raise Exception("Unclosed multi-line comment")
+    
+    def read_identifier(self):
+        """Read an identifier or keyword"""
         start_column = self.column
         id_str = ''
+        
         while self.current_char and (self.current_char.isalnum() or self.current_char == '_'):
-              id_str += self.current_char
-              self.advance()
+            id_str += self.current_char
+            self.advance()
+        
+        # Check for keywords
         keywords = {
             'let': 'let', 'in': 'in', 'fn': 'fn', 'where': 'where',
             'aug': 'aug', 'or': 'or', 'not': 'not', 'gr': 'gr',
@@ -82,98 +81,94 @@ class LexicalAnalyzer:
             'nil': 'nil', 'dummy': 'dummy', 'rec': 'rec',
             'within': 'within', 'and': 'and'
         }
+        
         if id_str in keywords:
             return Token(keywords[id_str], None, self.line, start_column)
         else:
             return Token('ID', id_str, self.line, start_column)
-        
+    
     def read_integer(self):
+        """Read an integer literal"""
         start_column = self.column
-        int_str = ''
+        num_str = ''
+        
         while self.current_char and self.current_char.isdigit():
-            int_str += self.current_char
+            num_str += self.current_char
             self.advance()
-        return Token('INT', int(int_str), self.line, start_column)
+        
+        return Token('INT', int(num_str), self.line, start_column)
     
-    def read_float(self):
-        start_column = self.column
-        float_str = ''
-        while self.current_char and (self.current_char.isdigit() or self.current_char == '.'):
-            float_str += self.current_char
-            self.advance()
-        return Token('FLOAT', float(float_str), self.line, start_column)
-    
-    #Read a string literal enclosed in double quotes
     def read_string(self):
+        """Read a string literal enclosed in double quotes"""
         start_column = self.column
-        string_str = ''
-        self.advance()
+        self.advance()  # consume opening quote
+        string_value = ''
+        
         while self.current_char and self.current_char != '"':
             if self.current_char == '\\':
                 self.advance()
                 if self.current_char == 'n':
-                    string_str += '\n'
+                    string_value += '\n'
                 elif self.current_char == 't':
-                    string_str += '\t'
-                elif self.current_char == '"':
-                    string_str += '"'
+                    string_value += '\t'
                 elif self.current_char == '\\':
-                    string_str += '\\'
+                    string_value += '\\'
+                elif self.current_char == '"':
+                    string_value += '"'
                 else:
-                    string_str += self.current_char
+                    string_value += self.current_char
             else:
-                string_str += self.current_char
+                string_value += self.current_char
             self.advance()
-
-            if self.current_char != '"':
-                raise Exception(f"Unclosed string literal at line {self.line}, column {start_column}")
-            self.advance()  # consume closing quote
-        return Token('STR', string_str, self.line, start_column)
+        
+        if self.current_char != '"':
+            raise Exception(f"Unclosed string literal at line {self.line}, column {start_column}")
+        
+        self.advance()  # consume closing quote
+        return Token('STR', string_value, self.line, start_column)
     
     def tokenize(self):
+        """Convert input text into a list of tokens"""
         while self.current_char:
             # Skip whitespace
             if self.current_char.isspace():
                 self.skip_whitespace()
                 continue
+            
             # Skip comments
             if self.current_char == '/' and self.peek() == '/':
-                self.advance()
-                self.advance()
+                self.advance()  # consume first '/'
+                self.advance()  # consume second '/'
                 self.skip_comment()
                 continue
-            # Skip multiline comments
+            
             if self.current_char == '/' and self.peek() == '*':
-                self.advance()
-                self.advance()
-                self.skip_multiline_comment()
+                self.advance()  # consume '/'
+                self.advance()  # consume '*'
+                self.handle_multi_line_comment()
                 continue
-            # Handle identifiers and keywords
+            
+            # Identifiers and keywords
             if self.current_char.isalpha() or self.current_char == '_':
-                token = self.identifier()
-                self.tokens.append(token)
+                self.tokens.append(self.read_identifier())
                 continue
-            # Handle integers
+            
+            # Integer literals
             if self.current_char.isdigit():
-                token = self.read_integer()
-                self.tokens.append(token)
+                self.tokens.append(self.read_integer())
                 continue
-            # Handle floats
-            if self.current_char == '.':
-                token = self.read_float()
-                self.tokens.append(token)
-                continue
-            # Handle string literals
+            
+            # String literals
             if self.current_char == '"':
-                token = self.read_string()
-                self.tokens.append(token)
+                self.tokens.append(self.read_string())
                 continue
-            # Handle operators
+            
+            # Operators and punctuation
             if self.current_char == '+':
-                token = Token('PLUS', None, self.line, self.column)
-                self.tokens.append(token)
+                self.tokens.append(Token('+', None, self.line, self.column))
                 self.advance()
                 continue
+            
             if self.current_char == '-':
                 self.tokens.append(Token('-', None, self.line, self.column))
                 self.advance()
@@ -181,6 +176,11 @@ class LexicalAnalyzer:
             
             if self.current_char == '*':
                 self.tokens.append(Token('*', None, self.line, self.column))
+                self.advance()
+                continue
+
+            if self.current_char == '>':
+                self.tokens.append(Token('>', None, self.line, self.column))
                 self.advance()
                 continue
             
@@ -263,46 +263,7 @@ class LexicalAnalyzer:
                 self.tokens.append(Token('}', None, self.line, self.column))
                 self.advance()
                 continue
-            if self.current_char == '!':
-                self.tokens.append(Token('!', None, self.line, self.column))
-                self.advance()
-                continue
-            if self.current_char == '<':
-                self.tokens.append(Token('<', None, self.line, self.column))
-                self.advance()
-                continue
-            if self.current_char == '>':
-                self.tokens.append(Token('>', None, self.line, self.column))
-                self.advance()
-                continue
-            if self.current_char == '?':
-                self.tokens.append(Token('?', None, self.line, self.column))
-                self.advance()
-                continue
-            if self.current_char == '^':
-                self.tokens.append(Token('^', None, self.line, self.column))
-                self.advance()
-                continue
-            if self.current_char == '%':
-                self.tokens.append(Token('%', None, self.line, self.column))
-                self.advance()
-                continue
-            if self.current_char == '~':
-                self.tokens.append(Token('~', None, self.line, self.column))
-                self.advance()
-                continue
-            if self.current_char == '#':
-                self.tokens.append(Token('#', None, self.line, self.column))
-                self.advance()
-                continue
-            if self.current_char == '$':
-                self.tokens.append(Token('$', None, self.line, self.column))
-                self.advance()
-                continue
-            if self.current_char == '\'':
-                self.tokens.append(Token('\'', None, self.line, self.column))
-                self.advance()
-                continue
+            
             # Multi-character operators
             if self.current_char == '-' and self.peek() == '>':
                 self.tokens.append(Token('->', None, self.line, self.column))
@@ -315,18 +276,16 @@ class LexicalAnalyzer:
         # Add EOF token
         self.tokens.append(Token('EOF', None, self.line, self.column))
         return self.tokens
+
+def tokenize_file(filename):
+    with open(filename, 'r') as file:
+        input_text = file.read()
     
-    
-   
-    
-def tokenize_file(self):
-    with open(self.text, 'r') as file:
-        self.text = file.read()
-    lexer = LexicalAnalyzer(self.text)
+    lexer = LexicalAnalyzer(input_text)
     return lexer.tokenize()
 
-    
-if __name__ == '__main__':
+# Test the lexical analyzer
+if __name__ == "__main__":
     import sys
     
     if len(sys.argv) != 2:
